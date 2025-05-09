cb0664942c065b4512109dc29f4323e6b7b9eb22
lianneg19 <125237077+lianneg19@users.noreply.github.com>
2025-05-09 09:23:37 -0500

    added string examples

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -1,9 +1,11 @@
+/*
 let age = 25;
 let year = 2019;
 
+// log things to console
 console.log(age, year);
 
-age = 30;
+ age = 30;
 console.log(age);
 
 const points = 100;
@@ -11,3 +13,35 @@ console.log(points);
 
 var score = 75; // old way, prefer not to use
 console.log(score);
+*/
+
+// strings
+console.log('hello, world');
+
+let email = 'mario@thenetninja.co.uk'
+console.log(email);
+
+// string concatnation
+let firstName = 'Brandon';
+let lastName = 'Sanderson';
+
+let fullName = firstName + ' ' + lastName;
+console.log(fullName);
+
+// getting characters
+console.log(fullName[2]);
+
+// string length
+console.log(fullName.length); // property
+
+// string methods
+// function = snippet of code that performs a specific task
+// method = is a kind of funtions associated with a particular object or data type
+console.log(fullName.toUpperCase());
+let result = fullName.toLowerCase();
+console.log(result, fullName)
+
+let index = email.indexOf('@');
+console.log(index);
+
+
