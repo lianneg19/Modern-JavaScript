cf6dc23da73e45c869d497fcf8284b82cf7d22f5
lgabriel20 <117782260+lgabriel20@users.noreply.github.com>
2025-05-12 09:15:26 -0500

    added string examples

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -59,4 +59,90 @@ let result = email.replace('n', 'w') // only the first n
 console.log(result);
 */
 
+/*
+let radius = 10;
+const pi = 3.14;
+
+// console.log(radius, pi);
+
+// math operatiors +, -, *, /, ** (power of), %
+// console.log(10 / 2);
+// let result = radius % 3;
+// let result = pi * radius**2;
+
+// order of operaion - B I D M A S
+// let result = 5 * (10-3)**2;
+
+// console.log(result);
+
+let likes = 10;
+
+// shorthand notations
+// likes  = likes + 1;
+// likes++;
+// likes--;
+// likes += 10;
+// likes -=5;
+// likes *= 2;
+// likes /= 2;
+
+// console.log(likes);
+
+//  NaN = not a number
+// console.log(5 / 'hello');
+// console.log(5 * 'hello');
+
+let result = 'the blog has ' + likes + ' likes';
+console.log(result);
+*/
+
+/*
+// template strings
+const title = 'Best reads of 2019';
+const author = 'Mario';
+const likes = 30;
+
+// concatnation way
+// let result = 'The blog called ' + title + ' by ' + author + ' has ' + likes + ' likes ';
+// let result = `The blog called ${title} by ${author} has ${likes} likes`;
+
+// console.log(result);
+
+// template string way
+// let result = `The blog called ${title} by ${author} has ${likes} likes`;
+
+// console.log(result);
+
+// creating html templates
+let html = `
+  <h2>${title}</h2>
+  <p>By ${author}</p>
+  <span>This blog has ${likes} likes</span>
+`;
+console.log(html);
+*/
+
+/*
+let ninjas = ['shaun', 'ryu', 'chun-li'];
+
+// ninjas[1] = 'ken';
+// console.log(ninjas[1]);
+
+// let ages = [20,25,30,35];
+// console.log(ages[2]);
+
+// let random = ['shaun', 'crystal', 30, 20];
+// // console.log(random);
+
+// console.log(ninjas.length);
+
+// array methods
+// let result = ninjas.join('_');
+// let result = ninjas.indexOf('chun-li');
+// let result = ninjas.concat(['ken', 'crystal']);
+// let result = ninjas.push('ken');
+// result = ninjas.pop();
+
+// console.log(result);
+*/
 
